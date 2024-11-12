f5fa249beab6a71c89479e42d9f3346c843ae052
difoxy2 <bigdk0900@gmail.com>
2024-11-12 22:08:13 +0800

    add "Add" function to gui.py

--- gui.py
+++ gui.py
@@ -2,8 +2,24 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text('Type in a to-do')
-input = sg.InputText(tooltip='Enter to-do')
+input = sg.InputText(tooltip='Enter to-do',key='todo',do_not_clear=False)
 button = sg.Button('Add')
-window = sg.Window('My to do app',layout=[[label],[input,button]])
-window.read()
+
+window = sg.Window('My to do app',
+                   layout=[[label],
+                           [input,button]], 
+                   font=('Verdana',20))
+
+while True:
+    event, values = window.read()
+    match event:
+        case 'Add':
+            if(len(values['todo'])>0):
+                functions.add_todo(values['todo'])
+
+        case WIN_CLOSED:
+            exit()
+    
+        
+
 window.close()
