f8cc060fdd3367809024e50da46b090103e5ed35
difoxy2 <bigdk0900@gmail.com>
2024-11-13 20:17:48 +0800

    add "Edit" and "Done" function to gui.py

--- gui.py
+++ gui.py
@@ -2,24 +2,47 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text('Type in a to-do')
-input = sg.InputText(tooltip='Enter to-do',key='todo',do_not_clear=False)
-button = sg.Button('Add')
+input = sg.InputText(tooltip='Enter to-do',key='input',do_not_clear=False)
+button_add = sg.Button('Add')
+listbox = sg.Listbox(functions.loaddata(),size=(40,10),key='listbox',enable_events=True)
+button_edit = sg.Button('Edit')
+button_complete = sg.Button('Done')
+
 
 window = sg.Window('My to do app',
-                   layout=[[label],
-                           [input,button]], 
+                   layout=[
+                        [label],
+                        [input,button_add],
+                        [listbox,sg.vtop(sg.Column([[button_edit], [button_complete],]))]
+                           ],
                    font=('Verdana',20))
 
 while True:
     event, values = window.read()
+    print(event)
+    print(values)
+
     match event:
         case 'Add':
-            if(len(values['todo'])>0):
-                functions.add_todo(values['todo'])
+            if(len(values['input'])>0):
+                functions.add_todo(values['input'])
+                listbox.update(values=functions.loaddata())
+        case 'listbox':
+            window['input'].update(value=values['listbox'][0].replace('\n',''))
+        case 'Edit':
+            list = functions.loaddata()
+            index = list.index(values['listbox'][0])
+            if(len(values['input'])>0):
+                functions.edit_todo(index,values['input'])
+                listbox.update(values=functions.loaddata())
+        case 'Done':
+            list = functions.loaddata()
+            index = list.index(values['listbox'][0])
+            functions.delete_todo(index)
+            listbox.update(values=functions.loaddata())
+            window['input'].update(value='')
 
         case WIN_CLOSED:
-            exit()
+            break
     
-        
-
 window.close()
