65f621a90e9ec24abbb76f0c47b76f96dd60b7ab
difoxy2 <bigdk0900@gmail.com>
2024-11-16 22:54:25 +0800

    add "Exit" function, generate .exe with pyinstaller

--- gui.py
+++ gui.py
@@ -7,13 +7,15 @@ button_add = sg.Button('Add')
 listbox = sg.Listbox(functions.loaddata(),size=(40,10),key='listbox',enable_events=True)
 button_edit = sg.Button('Edit')
 button_complete = sg.Button('Done')
+button_exit = sg.Button('Exit')
 
 
 window = sg.Window('My to do app',
                    layout=[
                         [label],
                         [input,button_add],
-                        [listbox,sg.vtop(sg.Column([[button_edit], [button_complete],]))]
+                        [listbox,sg.vtop(sg.Column([[button_edit], [button_complete],]))],
+                        [button_exit]
                            ],
                    font=('Verdana',20))
 
@@ -30,18 +32,26 @@ while True:
         case 'listbox':
             window['input'].update(value=values['listbox'][0].replace('\n',''))
         case 'Edit':
-            list = functions.loaddata()
-            index = list.index(values['listbox'][0])
-            if(len(values['input'])>0):
-                functions.edit_todo(index,values['input'])
-                listbox.update(values=functions.loaddata())
+            try:
+                list = functions.loaddata()
+                index = list.index(values['listbox'][0])
+                if(len(values['input'])>0):
+                    functions.edit_todo(index,values['input'])
+                    listbox.update(values=functions.loaddata())
+            except IndexError:
+                sg.popup('Select an item')
         case 'Done':
-            list = functions.loaddata()
-            index = list.index(values['listbox'][0])
-            functions.delete_todo(index)
-            listbox.update(values=functions.loaddata())
-            window['input'].update(value='')
-
+            try:
+                list = functions.loaddata()
+                index = list.index(values['listbox'][0])
+                functions.delete_todo(index)
+                listbox.update(values=functions.loaddata())
+                window['input'].update(value='')
+            except IndexError:
+                sg.popup('Select an item')
+        
+        case 'Exit':
+            break
         case WIN_CLOSED:
             break
     
